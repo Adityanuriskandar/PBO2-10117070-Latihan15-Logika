9a08c37c2898065cad62a583b9e0253a1afe14f7
Aditya Nur Iskandar <43404691+Adityanuriskandar@users.noreply.github.com>
2018-09-23 12:54:00 +0700

    Add files via upload

---
+++ PBO2-10117070-Latihan15-Logika/src/pbo2/pkg10117070/latihan15/logika/PBO210117070Latihan15Logika.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10117070.latihan15.logika;
+
+/**
+ *
+ * @author Aditya
+ * NAMA     : Aditya Nur Iskandar
+ * KELAS    : PBO2/IF2
+ * NIM      : 10117070
+ * Deskripsi Program : Program ini berisi program untuk Mengetahui Logika 
+ * Boolean.
+ */
+public class PBO210117070Latihan15Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+        System.out.println("a && b = " + (a&&b));
+        System.out.println("a || b = " + (a||b));
+        System.out.println("!a = " + !a);
+        System.out.println("!(a && b) = " + !(a && b));
+    }
+    
+}
